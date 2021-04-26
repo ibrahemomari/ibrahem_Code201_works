47f4354d09ef9dabbde55b52e8e6286550147f94
ibrahemomari <ibrahem.omari96@gmail.com>
2021-04-26 14:51:58 +0300

    My main page

---
+++ js/app.js
@@ -0,0 +1,118 @@
+'use strict';
+let Qus,Qus1,Qus2,Qus3,Qus4,Qus5,conf1,conf2,yesCount=0,noCount=0;
+
+alert("Welcome To My Page ");
+
+Qus=prompt("What your name please");
+
+conf1= confirm('Welcome agian '+Qus +' , are you ready to answes a few questions? ');
+
+
+switch(conf1)
+{
+    case true :
+        {
+            conf2=confirm("coooool , let's started (*^*), please use answer with Just yes or no ");
+            Qus1=prompt('Do you Know who am i ? (yes or no)');
+            
+            
+            
+            
+
+            if(Qus1.toUpperCase()==='YES' ||Qus1.toUpperCase()==='Y' )
+            {
+                yesCount++;
+                alert('correct answer ♥');
+            }
+            else if(Qus1.toUpperCase()==='NO' || Qus1.toUpperCase()==='N')
+            {
+                noCount++;
+                alert('Wrong answer ☻');
+            }
+            else
+            {
+                alert('inavalid answer');
+            }
+
+            Qus2=prompt('Do you Know how old I am? (yes or no)');
+            if(Qus2.toUpperCase()==='YES'||Qus2.toUpperCase()==='Y' )
+            {
+                yesCount++;
+                alert('correct answer ♥');
+            }
+            else if(Qus2.toUpperCase()==='NO' || Qus2.toUpperCase()==='N')
+            {
+                noCount++;
+                alert('Wrong answer ☻');
+            }
+            else
+            {
+                alert('inavalid answer');
+            }
+
+            
+            Qus3=prompt('Do you know what is my favorite football team ? (yes or no)');
+            if(Qus3.toUpperCase()==='YES'||Qus3.toUpperCase()==='Y' )
+            {
+                yesCount++;
+                alert('correct answer ♥');
+            }
+            else if(Qus3.toUpperCase()==='NO'|| Qus3.toUpperCase()==='N')
+            {
+                noCount++;
+                alert('Wrong answer ☻');
+            }
+            else
+            {
+                alert('inavalid answer');
+            }
+
+            Qus4=prompt('Do you know what is my favorite color ? (yes or no)');
+            if(Qus4.toUpperCase()==='YES'||Qus4.toUpperCase()==='Y' )
+            {
+                yesCount++;
+                alert('correct answer ♥');
+            }
+            else if(Qus4.toUpperCase()==='NO'|| Qus4.toUpperCase()==='N')
+            {
+                noCount++;
+                alert('Wrong answer ☻');
+            }
+            else
+            {
+                alert('inavalid answer');
+            }
+
+            Qus5=prompt('Do you know what is my role model ?(yes or no)');
+            if(Qus5.toUpperCase()==='YES'||Qus5.toUpperCase()==='Y' )
+            {
+                yesCount++;
+                alert('correct answer ♥');
+            }
+            else if(Qus5.toUpperCase()==='NO'|| Qus5.toUpperCase()==='N')
+            {
+                noCount++;
+                alert('Wrong answer ☻');
+            }
+            else
+            {
+                alert('inavalid answer');
+            }
+
+            console.log(yesCount);
+            console.log(noCount);
+            break;
+            
+
+
+        }
+    case false :
+        {
+            alert('Ok , you can continue browsing !!')
+        }
+            
+        
+}
+
+document.write('<h3>You answred '+yesCount+' questions with yes '+'</h3>');
+document.write('<h3>You answred '+noCount+' questions with no '+'</h3>');
